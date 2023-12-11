0898c2d1f6c091c499b0bf4aef7d401cbc34bf21
james-knott-iw <james.knott@integration.works>
2023-12-12 11:33:42 +1300

    initialise Hello world spring boot app

---
+++ hello-world/src/main/java/works/integration/helloworld/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+package works.integration.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
